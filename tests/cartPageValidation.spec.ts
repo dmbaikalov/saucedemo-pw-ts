a806c14ed8ba784586733907f1cd15d3f384a94d
Dmytro B <dmbaikalov@gmail.com>
2024-07-18 14:07:20 +0300

    Adding cart page validation tests with hooks and class instances

--- tests/cartPageValidation.spec.ts
+++ tests/cartPageValidation.spec.ts
@@ -17,12 +17,33 @@ test.describe('Cart Page Validations', () => {
         productPage = new ProductPage(page);
         cartPage = new CartPage(page);
   });
-    test('Validate that QTY column is visible', async () => {
+
+    test('QTY column in the cart is visible', async () => {
         await loginPage.open();
         await loginPage.fillLoginFormAndSubmit();
         await productsPage.cartButton.click();
         await expect(cartPage.qtyColumn).toBeVisible(); 
 });
+    test('Description column in the cart is visible', async () => {
+        await loginPage.open();
+        await loginPage.fillLoginFormAndSubmit();
+        await productsPage.cartButton.click();
+        await expect(cartPage.descriptionColumn).toBeVisible();
+    });
+
+    test('Continue shopping button in the cart is visible', async () => {
+        await loginPage.open();
+        await loginPage.fillLoginFormAndSubmit();
+        await productPage.cartButton.click();
+        await expect(cartPage.continueShoppingButton).toBeVisible();
+    });
+
+    test('Checkout button in the cart is visible', async () => {
+        await loginPage.open();
+        await loginPage.fillLoginFormAndSubmit();
+        await productPage.cartButton.click();
+        await expect(cartPage.checkoutButton).toBeVisible();
+    });
 
 });
 
