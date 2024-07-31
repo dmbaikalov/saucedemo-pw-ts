7dd2be7f52000b806c20c1b74703170bb8338f3d
Dmytro B <dmbaikalov@gmail.com>
2024-07-31 09:55:22 +0300

    make single beforeEach statement with class instances for all tests

--- tests/e2e.spec.ts
+++ tests/e2e.spec.ts
@@ -13,10 +13,17 @@ let checkoutPage: CheckoutPage;
 let productPage: ProductPage;
 let cartPage: CartPage;
 
+
+test.beforeEach(({ page }) => {
+    loginPage = new LoginPage(page);
+    productsPage = new ProductsPage(page);
+    productPage = new ProductPage(page);
+    cartPage = new CartPage(page);
+    checkoutPage = new CheckoutPage(page);
+    });
+
 test.describe('Login page validations', () => {
-    test.beforeEach(({ page }) => {
-        loginPage = new LoginPage(page);
-    })
+
     test('Username field is visible', async () => {
         await loginPage.open();
         await expect(loginPage.usernameField).toBeVisible();
@@ -31,15 +38,9 @@ test.describe('Login page validations', () => {
         await loginPage.open();
         await expect(loginPage.loginButton).toBeVisible();
     });
-
 });
 
 test.describe('Products page validations', () => {
-  test.beforeEach(({page}) => {
-    productsPage = new ProductsPage(page);
-    loginPage = new LoginPage(page);
-    productPage = new ProductPage(page);
-  });
 
   test('Validate that Log Out button is visible', async () => {
     await loginPage.open();
@@ -52,86 +53,75 @@ test.describe('Products page validations', () => {
     await loginPage.open();
     await loginPage.fillLoginFormAndSubmit();
     await expect(productsPage.cartIcon).toBeVisible();
-});
+  });
 
   test('Product name is visible on product page', async () => {
     await loginPage.open();
     await loginPage.fillLoginFormAndSubmit();
     await productsPage.itemsOnPage[1].click();
     await expect(productPage.productName).toBeVisible();
-});
+  });
+
   test('Product picture is visible on product page', async () => {
     await loginPage.open();
     await loginPage.fillLoginFormAndSubmit();
     await productsPage.itemsOnPage[1].click();
     await expect(productPage.productPricture).toBeVisible();
-});
+  });
 
   test('Product price is visible on product page', async () => {
     await loginPage.open();
     await loginPage.fillLoginFormAndSubmit();
     await productsPage.itemsOnPage[1].click();
     await expect(productPage.productPrice).toBeVisible();
-});
+  });
 
   test('Product description is visible on product page', async () => {
     await loginPage.open();
     await loginPage.fillLoginFormAndSubmit();
     await productsPage.itemsOnPage[1].click();
     await expect(productPage.productDescription).toBeVisible();
-});
+  });
 
   test('Cart button is visible on product page', async () => {
     await loginPage.open();
     await loginPage.fillLoginFormAndSubmit();
     await productsPage.itemsOnPage[1].click();
     await expect(productPage.addProductToCartButton).toBeVisible();
-});
+  });
 });
 
 test.describe('Cart Page Validations', () => {
-    test.beforeEach(({page}) => {
-        productsPage = new ProductsPage(page);
-        loginPage = new LoginPage(page);
-        productPage = new ProductPage(page);
-        cartPage = new CartPage(page);
+
+  test('QTY column in the cart is visible', async () => {
+      await loginPage.open();
+      await loginPage.fillLoginFormAndSubmit();
+      await productsPage.cartButton.click();
+      await expect(cartPage.qtyColumn).toBeVisible(); 
+  });
+  test('Description column in the cart is visible', async () => {
+      await loginPage.open();
+      await loginPage.fillLoginFormAndSubmit();
+      await productsPage.cartButton.click();
+      await expect(cartPage.descriptionColumn).toBeVisible();
   });
 
-    test('QTY column in the cart is visible', async () => {
-        await loginPage.open();
-        await loginPage.fillLoginFormAndSubmit();
-        await productsPage.cartButton.click();
-        await expect(cartPage.qtyColumn).toBeVisible(); 
-});
-    test('Description column in the cart is visible', async () => {
-        await loginPage.open();
-        await loginPage.fillLoginFormAndSubmit();
-        await productsPage.cartButton.click();
-        await expect(cartPage.descriptionColumn).toBeVisible();
-    });
+  test('Continue shopping button in the cart is visible', async () => {
+      await loginPage.open();
+      await loginPage.fillLoginFormAndSubmit();
+      await productPage.cartButton.click();
+      await expect(cartPage.continueShoppingButton).toBeVisible();
+  });
 
-    test('Continue shopping button in the cart is visible', async () => {
-        await loginPage.open();
-        await loginPage.fillLoginFormAndSubmit();
-        await productPage.cartButton.click();
-        await expect(cartPage.continueShoppingButton).toBeVisible();
-    });
-
-    test('Checkout button in the cart is visible', async () => {
-        await loginPage.open();
-        await loginPage.fillLoginFormAndSubmit();
-        await productPage.cartButton.click();
-        await expect(cartPage.checkoutButton).toBeVisible();
-    });
-
+  test('Checkout button in the cart is visible', async () => {
+      await loginPage.open();
+      await loginPage.fillLoginFormAndSubmit();
+      await productPage.cartButton.click();
+      await expect(cartPage.checkoutButton).toBeVisible();
+  });
 });
 
 test.describe('E2E tests', () => {
-  test.beforeEach(({ page }) => {
-    loginPage = new LoginPage(page);
-    productsPage = new ProductsPage(page);
-    checkoutPage = new CheckoutPage(page);
-  });
 
   test('User can login on the website', async () => {
     await loginPage.open();
@@ -145,9 +135,7 @@ test.describe('E2E tests', () => {
     await productsPage.addItemToCart[0].click();
     await expect(productsPage.cartButton).toHaveText('1');
   });
-  
 // TO-DO --> Separate duplicates with different methods
-  
   test('After adding six products to the cart, the user will be able to see the number six on the cart icon', async () => {
     await loginPage.open();
     await loginPage.fillLoginFormAndSubmit();
@@ -173,8 +161,6 @@ test.describe('E2E tests', () => {
     await checkoutPage.finishButton.click();
     await expect(checkoutPage.checkOutIsComplete).toBeVisible();
   });
-
-
 });
 
 
